567c68f1c16d0236e1f6046b423d90994bc72348
Sarvesh Agarwal <sarveshiitb@gmail.com>
2014-12-19 12:31:06 +0530

    "First commit for api code, copy of feature/user-food-history"

--- public_html/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo md5(uniqid(rand(0,0), true));
-
-?>
--- public_html/view_files/uploaded_reports.inc.php
+++
@@ -1,4 +0,0 @@
-<div class="dvFloat formpadding1" id="dvUploadedGallery" name="dvUploadedGallery" style="margin-bottom:5px; border-bottom:0px;">
-		&nbsp;
- </div>
-
